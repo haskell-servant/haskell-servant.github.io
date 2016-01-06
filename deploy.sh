15f89f3cd4e633cce0ba6e641658e77a84620697
Julian K. Arni <jkarni@gmail.com>
2016-01-06 15:09:02 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,12 @@ COMMIT=$(git rev-parse HEAD)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 MSG="Built from $COMMIT"
 
-SERVANT_VERSIONS=(0.2 0.4 0.5)
+SERVANT_VERSIONS=(versions/0.2 versions/0.4 versions/0.5)
+MAIN_BRANCH="main"
 
 # Sanity check: documentation should only be built for one of the known major
 # versions of servant.
-check_branch_is_correct () {
+check_tutorial_branch_is_correct () {
     if [[ " ${SERVANT_VERSIONS[@]} " =~ " ${BRANCH} " ]]; then
         # Check that version of servant used in cabal file corresponds to the
         # branch we are on.
@@ -23,16 +24,32 @@ check_branch_is_correct () {
     fi
 }
 
-if [ ! -d $SERVANT_WWW ]; then
-    git clone $REPO_URL $HOME/.servant-www
-    echo "Created directory $SERVANT_WWW"
-fi
+# Create directory to use for build
+prepare_deploy () {
+    if [ ! -d $SERVANT_WWW ]; then
+        git clone $REPO_URL $HOME/.servant-www
+        echo "Created directory $SERVANT_WWW"
+    fi
+    git rm -r $SERVANT_WWW/*
+}
 
-cd $SERVANT_WWW
-git rm -r ./*
-cp -R $SITE/tutorial/* ./tutorial/${BRANCH}/
-git add ./**
-git commit -m "Built tutorial for version $BRANCH from $COMMIT"
-git push origin master
-echo $MSG
-cd $CURRDIR
+# Deploy tutorial
+deploy_tutorial_version () {
+    check_tutorial_branch_is_correct
+    prepare_deploy
+    cp -R $SERVANT_WWW/$SITE/tutorial/* $SERVANT_WWW/tutorial/$BRANCH
+    git add $SERVANT_WWW/tutorial/$BRANCH
+    git commit -m "Built tutorial for version $BRANCH from $COMMIT"
+    git push origin master
+}
+
+deploy_main_version () {
+    # TODO
+}
+
+
+if [[ "${BRANCH}" = "${MAIN_BRANCH}" ]] ; then
+    deploy_main_version
+else
+    deploy_tutorial_version
+fi
