2534606c2578c9ca456f003ed3d1f5322a878899
Sönke Hahn <soenkehahn@gmail.com>
2016-01-13 23:31:33 +0100

    compile tutorial: converted docs.lhs

--- check-code/check-code.sh
+++ check-code/check-code.sh
@@ -6,3 +6,4 @@ cabal exec -- ghc -outputdir build-output ../tutorial/api-type.lhs -O0 -c
 cabal exec -- ghc -outputdir build-output ../tutorial/server.lhs -O0 -c -fno-warn-missing-methods
 cabal exec -- ghc -outputdir build-output ../tutorial/client.lhs -O0 -c -fno-warn-missing-methods
 cabal exec -- ghc -outputdir build-output ../tutorial/javascript.lhs -O0 -c -fno-warn-missing-methods
+cabal exec -- ghc -ibuild-output -outputdir build-output ../tutorial/docs.lhs -O0 -c -fno-warn-missing-methods
