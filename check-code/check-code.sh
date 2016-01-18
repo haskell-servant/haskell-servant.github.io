a682472edfd873886b5b1fc0b4b858ae51661e89
Sönke Hahn <soenkehahn@gmail.com>
2016-01-18 23:40:11 +0100

    compile examples: small refactoring

--- check-code/check-code.sh
+++ check-code/check-code.sh
@@ -2,8 +2,10 @@
 
 set -o errexit
 
-cabal exec -- ghc -outputdir build-output ../tutorial/api-type.lhs -O0 -c
-cabal exec -- ghc -outputdir build-output ../tutorial/server.lhs -O0 -c -fno-warn-missing-methods
-cabal exec -- ghc -outputdir build-output ../tutorial/client.lhs -O0 -c -fno-warn-missing-methods
-cabal exec -- ghc -outputdir build-output ../tutorial/javascript.lhs -O0 -c -fno-warn-missing-methods
-cabal exec -- ghc -ibuild-output -outputdir build-output ../tutorial/docs.lhs -O0 -c -fno-warn-missing-methods
+tinc
+
+cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/api-type.lhs -O0 -c
+cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/server.lhs -O0 -c -fno-warn-missing-methods -fno-warn-name-shadowing
+cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/client.lhs -O0 -c -fno-warn-missing-methods -fno-warn-name-shadowing
+cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/javascript.lhs -O0 -c -fno-warn-missing-methods
+cabal exec -- ghc -Wall -Werror -ibuild-output -outputdir build-output ../tutorial/docs.lhs -O0 -c -fno-warn-missing-methods
