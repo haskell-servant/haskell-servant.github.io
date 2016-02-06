cc2dc597ea1968bc92197f71463c86edb7ca459e
Julian K. Arni <jkarni@gmail.com>
2016-02-06 16:19:19 +0100

    Fix check-code

--- check-code/check-code.sh
+++ check-code/check-code.sh
@@ -9,4 +9,4 @@ cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/server.lhs -
 cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/client.lhs -O0 -c -fno-warn-missing-methods -fno-warn-name-shadowing
 cabal exec -- ghc -Wall -Werror -outputdir build-output ../tutorial/javascript.lhs -O0 -c -fno-warn-missing-methods
 cabal exec -- ghc -Wall -Werror -ibuild-output -outputdir build-output ../tutorial/docs.lhs -O0 -c -fno-warn-missing-methods
-cabal exec -- runghc -Wall -Werror -fno-warn-missing-methods ../posts/2016-01-21-servant-swagger.lhs -O0 -pgmL markdown-unlit -c
+cabal exec -- runghc -Wall -Werror -fno-warn-missing-methods ../posts/2016-02-06-servant-swagger.lhs -O0 -pgmL markdown-unlit -c
