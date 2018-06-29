529aa45121c56462fc9445fd3509fde739b17b8f
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-06-29 22:44:03 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,27 @@
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+SERVANT_WWW="$XDG_CACHE_HOME/haskell-servant-github-io"
+
 REPO_URL="git@github.com:haskell-servant/haskell-servant.github.io"
-SERVANT_WWW="$HOME/.servant-www"
 SITE="$PWD/_site"
-CURRDIR="$PWD"
-BIN="dist/build/site/site"
-COMMIT=`git rev-parse HEAD`
-MSG="Built from $COMMIT"
+COMMIT="$(git rev-parse HEAD)"
 
 set -o errexit
 
-if [ ! -d $SERVANT_WWW ]; then
-	git clone $REPO_URL $HOME/.servant-www
+# Build locally
+cabal new-run -w ghc-8.4.3 site build
+
+# Clone dir
+if [ ! -d "$SERVANT_WWW" ]; then
+	git clone "$REPO_URL" "$SERVANT_WWW"
 	echo "Created directory $SERVANT_WWW"
 fi
 
-cd $SERVANT_WWW
+
+cd "$SERVANT_WWW"
 git checkout master
 git rm -r ./*
-cp -R $SITE/* ./ 
+cp -R "$SITE"/* ./
 git add ./**
 git commit -m "Built from $COMMIT"
 git push origin master
-echo $MSG
-cd $CURRDIR
+echo "Build from $COMMIT"
